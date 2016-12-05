259626684ef89dabdb07c291a36ee83c4a6dd693
Travis Stanley <travisstanley@Admins-MacBook-Air.local>
2016-12-05 12:39:18 -0800

    create some controllers and models for users and sessions

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,2 +1,9 @@
 class Episode < ApplicationRecord
+
+	belongs_to :category
+	belongs_to :user
+
+	validates :title, presence: true, uniqueness: true
+	validates :description, presence: true, uniqueness: {scope: :title}
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	
+	
+    #home page
+    root "home#index"
+
+
 end
